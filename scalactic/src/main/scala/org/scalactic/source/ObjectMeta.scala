609dc646eafa12893eb4127891a87da46a5f0c02
Chua Chee Seng <cheeseng@amaseng.com>
2017-12-06 11:32:03 +0800

    Making special edition of ObjectMeta for scala 2.10, as scala reflection in scala 2.10 isn't thread safe.

--- scalactic/src/main/scala/org/scalactic/source/ObjectMeta.scala
+++ scalactic/src/main/scala/org/scalactic/source/ObjectMeta.scala
@@ -15,6 +15,8 @@
  */
 package org.scalactic.source
 
+import org.scalactic.ScalacticVersions
+
 trait ObjectMeta {
 
   def fieldNames: scala.collection.immutable.IndexedSeq[String]
@@ -35,31 +37,31 @@ object ObjectMeta {
 
   def apply(v: Any): ObjectMeta = {
 
-    val typeMirror = runtimeMirror(v.getClass.getClassLoader)
-    val instanceMirror = typeMirror.reflect(v)
-    val symbol: ClassSymbol = instanceMirror.symbol
+    /*new ObjectMeta {
 
-    def mapCaseAccessor(s: Symbol): Option[String] = {
-      s match {
-        case m: MethodSymbol =>
-          if (m.isCaseAccessor)
-            Some(s.name.toString)
-          else
+      val typeMirror = runtimeMirror(v.getClass.getClassLoader)
+      val instanceMirror = typeMirror.reflect(v)
+      val symbol: ClassSymbol = instanceMirror.symbol
+
+      def mapCaseAccessor(s: Symbol): Option[String] = {
+        s match {
+          case m: MethodSymbol =>
+            if (m.isCaseAccessor)
+              Some(s.name.toString)
+            else
+              None
+
+          case m: TermSymbol if m.isVal =>
+            val name = m.name.toString.trim
+            if (m.name.toString.endsWith("$mcI$sp"))
+              Some(name.substring(0, name.length - 7))
+            else
+              Some(name)
+
+          case other =>
             None
-
-        case m: TermSymbol if m.isVal =>
-          val name = m.name.toString.trim
-          if (m.name.toString.endsWith("$mcI$sp"))
-            Some(name.substring(0, name.length - 7))
-          else
-            Some(name)
-
-        case other =>
-          None
+        }
       }
-    }
-
-    new ObjectMeta {
 
       val caseAccessorSymbols = symbol.toType.declarations
 
@@ -99,7 +101,161 @@ object ObjectMeta {
             throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
         }
       }
+    }*/
+
+    def mapCaseAccessor(s: Symbol): Option[String] = {
+      s match {
+        case m: MethodSymbol =>
+          if (m.isCaseAccessor)
+            Some(s.name.toString)
+          else
+            None
+
+        case m: TermSymbol if m.isVal =>
+          val name = m.name.toString.trim
+          if (m.name.toString.endsWith("$mcI$sp"))
+            Some(name.substring(0, name.length - 7))
+          else
+            Some(name)
+
+        case other =>
+          None
+      }
     }
+
+    // Scala reflection in scala 2.10 is not thread safe, and not reliable to use, see:
+    // https://issues.scala-lang.org/browse/SI-6240
+    // so for scala 2.10 we'll synchronized the code to call scala reflection and build the fields
+    // and values map eagerly.
+    if (ScalacticVersions.BuiltForScalaVersion == "2.10") {
+      val (fieldNameList, valueMap, typeNameMap, shortTypeNameMap) =
+        synchronized {
+          val typeMirror = runtimeMirror(v.getClass.getClassLoader)
+          val instanceMirror = typeMirror.reflect(v)
+          val symbol: ClassSymbol = instanceMirror.symbol
+
+          val caseAccessorSymbols = symbol.toType.declarations
+
+          val fieldNames: Vector[String] = {
+            symbol.toType.declarations.flatMap(mapCaseAccessor).toVector.distinct
+          }
+
+          val valueMap: Map[String, Any] =
+            Map.empty[String, Any] ++
+              fieldNames.map { name =>
+                caseAccessorSymbols.find(s => (s.name.toString == name || s.name.toString == (name + "$mcI$sp")) && (s.isMethod || s.isTerm)) match {
+                  case Some(fieldSymbol) =>
+                    val fieldMirror = instanceMirror.reflectField(fieldSymbol.asTerm)
+                    (name, fieldMirror.get)
+
+                  case None =>
+                    throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+                }
+              }
+
+          val typeNameMap: Map[String, String] =
+            Map.empty[String, String] ++
+              fieldNames.map { name =>
+                caseAccessorSymbols.find(s => s.name.toString == name) match {
+                  case Some(fieldSymbol) =>
+                    // This is a safe cast
+                    (name, fieldSymbol.asInstanceOf[MethodSymbol].returnType.typeSymbol.fullName)
+                    //(name, fieldSymbol.asInstanceOf[TermSymbol].asType.fullName)
+
+                  case None =>
+                    throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+                }
+              }
+
+          val shortTypeNameMap: Map[String, String] =
+            Map.empty[String, String] ++
+              fieldNames.map { name =>
+                caseAccessorSymbols.find(s => s.name.toString == name) match {
+                  case Some(fieldSymbol) =>
+                    // This is a safe cast
+                    (name, fieldSymbol.asInstanceOf[MethodSymbol].returnType.typeSymbol.name.decoded)
+
+                  case None =>
+                    throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+                }
+              }
+
+          (fieldNames, valueMap, typeNameMap, shortTypeNameMap)
+        }
+
+      new ObjectMeta {
+
+        val fieldNames = fieldNameList
+
+        def value(name: String): Any =
+          valueMap.get(name) match {
+            case Some(value) => value
+            case None =>
+              throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+          }
+
+        def typeName(name: String): String =
+          typeNameMap.get(name) match {
+            case Some(value) => value
+            case None =>
+              throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+          }
+
+        def shortTypeName(name: String): String =
+          shortTypeNameMap.get(name) match {
+            case Some(value) => value
+            case None =>
+              throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+          }
+      }
+    }
+    else
+      new ObjectMeta {
+
+        val typeMirror = runtimeMirror(v.getClass.getClassLoader)
+        val instanceMirror = typeMirror.reflect(v)
+        val symbol: ClassSymbol = instanceMirror.symbol
+
+        val caseAccessorSymbols = symbol.toType.declarations
+
+        lazy val fieldNames = {
+          symbol.toType.declarations.flatMap(mapCaseAccessor).toVector.distinct
+        }
+
+        def value(name: String): Any = {
+          caseAccessorSymbols.find(s => (s.name.toString == name || s.name.toString == (name + "$mcI$sp")) && (s.isMethod || s.isTerm)) match {
+            case Some(fieldSymbol) =>
+              val fieldMirror = instanceMirror.reflectField(fieldSymbol.asTerm)
+              fieldMirror.get
+
+            case None =>
+              throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+          }
+        }
+
+        def typeName(name: String): String = {
+          caseAccessorSymbols.find(s => s.name.toString == name) match {
+            case Some(fieldSymbol) =>
+              // This is a safe cast
+              fieldSymbol.asInstanceOf[MethodSymbol].returnType.typeSymbol.fullName
+
+            case None =>
+              throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+          }
+        }
+
+        def shortTypeName(name: String): String = {
+          caseAccessorSymbols.find(s => s.name.toString == name) match {
+            case Some(fieldSymbol) =>
+              // This is a safe cast
+              fieldSymbol.asInstanceOf[MethodSymbol].returnType.typeSymbol.name.decoded
+
+            case None =>
+              throw new IllegalArgumentException("'" + name + "' is not attribute for this instance.")
+          }
+        }
+      }
+
   }
 
 }
