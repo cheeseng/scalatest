9ab580afcc56a977eef67ac4b6e6303f6c310838
Chua Chee Seng <cheeseng@amaseng.com>
2020-07-26 14:23:55 +0800

    Changed release and previous version, upgrade to mima 0.7.0.

--- project/BuildCommons.scala
+++ project/BuildCommons.scala
@@ -7,9 +7,9 @@ trait BuildCommons {
 
   lazy val supportedScalaVersions = List("2.13.2", "2.12.11", "2.11.12", "2.10.7")
 
-  val releaseVersion = "3.2.0"
+  val releaseVersion = "3.2.1"
 
-  val previousReleaseVersion = "3.1.2"
+  val previousReleaseVersion = "3.2.0"
 
   def rootProject: Project
 
